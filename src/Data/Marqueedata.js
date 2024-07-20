76099cf4cfc48fee3a08e2cb789e371de196ed1d
aditya <asadityasingh2572000@gmail.com>
2024-07-20 16:14:06 +0530

    add new news for today

---
+++ src/Data/Marqueedata.js
@@ -0,0 +1,19 @@
+import mnews1 from "../assets/newsImages/news1.jpeg";
+import mnews2 from "../assets/newsImages/news4.jpeg";
+import mnews3 from "../assets/newsImages/news5.jpeg";
+import mnews4 from "../assets/newsImages/news8.jpeg";
+import n1 from "../assets/newsImages/n-1.jpeg";
+import n2 from "../assets/newsImages/n-2.jpeg";
+import n3 from "../assets/newsImages/n-3.jpeg";
+
+
+export const marqureedata = [
+  { imgs: mnews1, conetnt:"बैंक लूट आरोपी गिरफ्तार" },
+  { imgs: mnews2, conetnt:"उज्जैन बीती रात गोली चलने से हड़कंप" },
+  { imgs: mnews3, conetnt:"CM मोहन का बड़ा ऐलान" },
+  { imgs: mnews4, conetnt:"मध्यप्रदेश राज्यसभा : केपी यादव को झटका" },
+  { imgs: n1, conetnt:"आदरणीय मुख्यमंत्री जी भाजपा द्वारा जात और धर्म..." },
+  { imgs: n2, conetnt:"देवास एबी रोड मक्सी रोड पर ग्राम टोंक कला की पुलिया से ट्रक गिरा " },
+  { imgs: n3, conetnt:"स्लग:रीवा में सेल्फी के चक्कर में जलप्रपात में गिरी महिलाः" },
+
+]
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import CollapsibleExample from "./Navbar";
 import logo from "./assets/plogo.png";
-import pc from "./assets/newsImages/news1.jpeg";
-import sarfira  from "./assets/newsImages/news4.jpeg";
-import luck  from "./assets/newsImages/news5.jpeg";
-import mp  from "./assets/newsImages/news8.jpeg";
+import { marqureedata } from "./Data/Marqueedata";
 
 import Newscard from "./Newscard";
 import styless from "./Header.module.css";
@@ -24,31 +21,15 @@ const Header = () => {
         <div className="border-1 border-bottom mt-3 position-relative">
           <div className={styless.taza}> ताज़ा खबर</div>
           <marquee>
-            <div className="d-flex justify-content-evenly fs-5">
-              <div>
-                {" "}
-                <img src={pc} className={styless.m_img} /> बैंक लूट आरोपी गिरफ्तार
-              </div>
-              <div className="ms-4">
-                <img src={sarfira} className={styless.m_img} 
-                 />
-                उज्जैन बीती रात गोली चलने से हड़कंप
-              </div>
-
-              <div className="ms-4">
-                <img src={luck} className={styless.m_img} />
-                CM मोहन का बड़ा ऐलान
-              </div>
-
-              <div className="ms-4">
-                <img src={mp} className={styless.m_img} />
-                मध्यप्रदेश राज्यसभा : केपी यादव को झटका
-              </div>
-
-              <div className="ms-4">
-                <img src={luck} className={styless.m_img} />
-                Launching Soon Launching Soon Launching Soon
-              </div>
+            <div className="d-flex justify-content-evenly fs-6 position-relative">
+              {marqureedata.map((items, index) => (
+                <div className="ms-2" key={index}>
+                  <div className={styless.img_container}>
+                    <img src={items.imgs} className={styless.m_img} />
+                    <div className="mt-2 mx-2">{items.conetnt}</div>
+                  </div>
+                </div>
+              ))}
             </div>
           </marquee>
         </div>
